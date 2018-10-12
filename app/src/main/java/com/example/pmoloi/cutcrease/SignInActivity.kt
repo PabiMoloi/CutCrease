3f2cee28add804b0def97a2e2cf29006718e38ae
Pabi Moloi <moloipaballo@gmail.com>
2018-10-12 07:57:03 +0200

    Refactor.

--- app/src/main/java/com/example/pmoloi/cutcrease/SignInActivity.kt
+++ app/src/main/java/com/example/pmoloi/cutcrease/SignInActivity.kt
@@ -2,13 +2,21 @@ package com.example.pmoloi.cutcrease
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Visibility
+import android.view.View
+import android.widget.ProgressBar
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        //val toolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(findViewById(R.id.toolbarSignUp))
+    }
+
+    fun onSignInClick(view: View){
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar2)
+        progressBar.visibility = View.VISIBLE
+        view.isEnabled = false
     }
 }
