64469a144656ea564a56c63943b64d96ab125f60
Pabi Moloi <moloipaballo@gmail.com>
2018-10-12 08:05:01 +0200

    Refactor.

--- app/src/main/java/com/example/pmoloi/cutcrease/SignInActivity.kt
+++ app/src/main/java/com/example/pmoloi/cutcrease/SignInActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.Visibility
 import android.view.View
+import android.widget.EditText
 import android.widget.ProgressBar
 
 class SignInActivity : AppCompatActivity() {
@@ -16,7 +17,11 @@ class SignInActivity : AppCompatActivity() {
 
     fun onSignInClick(view: View){
         val progressBar = findViewById<ProgressBar>(R.id.progressBar2)
+        val usernameTextField = findViewById<EditText>(R.id.editTextSignInUsername)
+        val passwordTextField = findViewById<EditText>(R.id.editTextSignInPassword)
         progressBar.visibility = View.VISIBLE
+        usernameTextField.isEnabled = false
+        passwordTextField.isEnabled = false
         view.isEnabled = false
     }
 }
